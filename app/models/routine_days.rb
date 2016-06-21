661c884879803793a4dfb30b8f2d6b60feb6524f
Savannah Scott <sascott624@gmail.com>
2016-06-21 19:10:29 +0000

    created routine_tasks model, routine_days model

---
+++ app/models/routine_days.rb
@@ -0,0 +1,6 @@
+class RoutineDay < ActiveRecord::Base
+
+  belongs_to :routine
+  belongs_to :day
+
+end
---
+++ app/models/routine_tasks.rb
@@ -0,0 +1,6 @@
+class RoutineTask < ActiveRecord::Base
+
+  belongs_to :routine
+  belongs_to :task
+
+end
